2f3a4ea62a55a46ceb198e8ea0f6c4d4c774cc53
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2024-02-27 02:09:07 -0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Card/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('111111')
+</script>
+
+<template>
+    <p>{{ text }}</p>
+</template>
